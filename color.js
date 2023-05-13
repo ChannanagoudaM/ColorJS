8afbff30992b42390a98e4e20c1bd67361327423
ChannanagoudaM <129206262+ChannanagoudaM@users.noreply.github.com>
2023-05-14 00:42:48 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,8 @@
+let button=document.getElementById('button');
+button.addEventListener('click',showMsg);
+function showMsg()
+{
+    // const number=Math.floor(Math.random()*16777215);
+    // const code ="@"+number.toString(16);
+    document.getElementById('t').style.backgroundColor=rgb(255, 99, 71)
+}
